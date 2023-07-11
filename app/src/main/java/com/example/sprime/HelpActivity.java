6eebb79a1fe5f61440c9a46cee79dbd77585eb0e
karl yarfoh <130949896+yarfoh@users.noreply.github.com>
2023-07-11 17:34:33 +0000

    Update HelpActivity.java

--- app/src/main/java/com/example/sprime/HelpActivity.java
+++ app/src/main/java/com/example/sprime/HelpActivity.java
@@ -12,7 +12,7 @@ public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
+        setContentView(R.layout.activity_help); 
 
     }
 }
